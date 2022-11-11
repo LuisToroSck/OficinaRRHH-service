66f79176172c4f26c510f1c1e9d094923552d431
Luis Javier Toro Flores <91231015+LuisToroSck@users.noreply.github.com>
2022-11-11 17:20:35 -0300

    Copia de microservicio en Maven

---
+++ oficinarrhh-service/src/main/java/com/example/oficinarrhhservice/OficinarrhhServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.oficinarrhhservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OficinarrhhServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OficinarrhhServiceApplication.class, args);
+	}
+
+}
